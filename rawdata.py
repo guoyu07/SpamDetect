bfabaa8238662698df19d8080d31c433b7598aa5
shisy13 <shisy13@icloud.com>
2016-05-29 15:35:29 +0800

    rawdata down

--- rawdata.py
+++ rawdata.py
@@ -3,6 +3,8 @@
 import jieba
 import os
 
+jieba.enable_parallel()
+
 
 def convert2utf8(srcf, tarf):
     sf = open(srcf, 'r')
@@ -45,6 +47,51 @@ def readEmail(filename):
         info[po].append(line)
     return info
 
-# info = readEmail('trec06c/utf8/000/000')
 
-# print ','.join(jieba.cut(''.join(info[1]))).encode('utf-8')
+def email2dict(filename):
+    info = readEmail(filename)
+    words = jieba.cut(''.join(info[1])
+                      .replace('。', '。' + os.linesep)
+                      .replace('，', '，' + os.linesep)
+                      .replace('；', '；' + os.linesep)
+                      .replace('、', '、' + os.linesep)
+                      .replace('！', '！' + os.linesep)
+                      .replace('？', '？' + os.linesep))
+    wordcnt = {}
+    for word in words:
+        word = word.encode('utf-8')
+        if word in wordcnt:
+            wordcnt[word] = wordcnt[word] + 1
+        else:
+            wordcnt[word] = 1
+    return wordcnt
+
+
+def data2vec(indexfile, max=-1):
+    f = file(indexfile, 'r')
+    lines = f.readlines()
+    lines = [line.strip().split(' ') for line in lines]
+    if max == -1:
+        max = len(lines)
+
+    vectors = [[], []]
+    cnt = 0
+    for line in lines:
+        emailvec = []
+        positive = 1 if line[0].lower() == 'spam' else 0
+        emailvec.append(positive)
+
+        path = os.path.join('trec06c/utf8/', '/'.join(line[1].split('/')[-2:]))
+        worddict = email2dict(path)
+
+        emailvec.append(worddict)
+        vectors[positive].append(emailvec)
+        cnt += 1
+        if cnt >= max:
+            break
+    return vectors
+
+
+
+vecs = data2vec('trec06c/full/index', 1000)
+print len(vecs[0]), len(vecs[1])
